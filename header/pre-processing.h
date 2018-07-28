2bd5020d9ecf223afeb11dfc2c95fd8ae6950d9a
batburger <a_modenbach@outlook.com>
2018-07-29 00:12:07 +0200

    Split into pre-processing-detail.h & pipes.h.

--- header/pre-processing.h
+++ header/pre-processing.h
@@ -6,68 +6,16 @@
 #define VM2_0_COMPILER_PRE_PROCESSING_H
 
 #include <string>
+#include "pre-processing-detail.h"
+#include "pipes.h"
 #include "InputFile.h"
 #include "File.h"
 
 namespace compiler::pre_processing {
-    namespace details {
-        std::vector<compiler::data::InputFile>
-        fetchIncludeFiles(compiler::data::InputFile file);
-
-        std::vector<compiler::data::InputFile>
-        fetchIncludeFiles(compiler::data::InputFile file,
-                          std::vector<compiler::data::InputFile> files);
-
-        compiler::data::File
-        merge_include_files(std::vector<compiler::data::File> files);
-
-        std::vector<compiler::data::File>
-        makeSymbolsUnique(std::vector<compiler::data::File> files);
-
-
-        std::vector<compiler::data::File>
-        transformInputFiles(std::vector<compiler::data::InputFile> files);
-
-        struct fetchIncludeFiles_f {
-            template<class T>
-            auto operator()(T x) const {
-                    return fetchIncludeFiles(x);
-            }
-        };
-
-        struct merge_include_files_f {
-            template<class T>
-            auto operator()(T x) const {
-                    return merge_include_files(x);
-            }
-        };
-
-        struct makeSymbolsUnique_f {
-            template<class T>
-            auto operator()(T x) const {
-                    return makeSymbolsUnique(x);
-            }
-        };
-
-        struct transformInputFiles_f {
-            template<class T>
-            auto operator()(T x) const {
-                    return transformInputFiles(x);
-            }
-        };
-
-        template<class F>
-        struct pipe_closure : F {
-            template<class... Xs>
-            pipe_closure(Xs&&... xs) : F(std::forward<Xs>(xs)...)
-            {}
-        };
-    }
-
-    const details::pipe_closure<details::fetchIncludeFiles_f> fetchIncludeFiles = {};
-    const details::pipe_closure<details::merge_include_files_f> merge_include_files = {};
-    const details::pipe_closure<details::makeSymbolsUnique_f> makeSymbolsUnique = {};
-    const details::pipe_closure<details::transformInputFiles_f> transformInputFiles = {};
+    const utils::pipe_closure<details::fetchIncludeFiles_f> fetchIncludeFiles = {};
+    const utils::pipe_closure<details::merge_include_files_f> merge_include_files = {};
+    const utils::pipe_closure<details::makeSymbolsUnique_f> makeSymbolsUnique = {};
+    const utils::pipe_closure<details::transformInputFiles_f> transformInputFiles = {};
 
     void check_existence_of_all_files$(compiler::data::InputFile file);
 
@@ -75,10 +23,6 @@ namespace compiler::pre_processing {
                                        std::vector<compiler::data::InputFile> includeStack);
 
 }
-template <class T, class F>
-decltype(auto) operator| (T&& x, const compiler::pre_processing::details::pipe_closure<F>& p){
-    return p(std::forward<T>(x));
-};
 
 
 #endif //VM2_0_COMPILER_PRE_PROCESSING_H
